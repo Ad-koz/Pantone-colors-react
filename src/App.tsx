4e35bba65ac2f75dacb9ab23e8f5869eeb220edc
Ada <adrkozlo2@gmail.com>
2023-01-31 21:23:05 +0100

    Search bar and reload button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DataAPI from './components/DataAPI/DataAPI';
 import ProductsTab from './components/ProductsTab/ProductsTab';
+
 import { ListItem, Card, ListItemText } from "@mui/material";
 import TabHead from './components/TabHead/TabHead';
 import TableContainer from '@mui/material/TableContainer';
@@ -8,9 +9,11 @@ import Table from '@mui/material/Table';
 import Paper from '@mui/material/Paper';
 
 
+
 function App() {
   return (
     <div className="App">
+     
       <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
       <TabHead></TabHead>
--- src/components/DataAPI/DataAPI.tsx
+++ src/components/DataAPI/DataAPI.tsx
@@ -15,8 +15,14 @@ export interface ProductsObj
 
 
 const DataAPI = () => {
-
-  const [products, setProducts] = useState<ProductsObj[] | []>([])
+  function refreshPage() {
+    window.location.reload();
+  }
+  const [products, setProducts] = useState<ProductsObj[] | []>([]);
+  const [query, setQuery] = useState("");
+  const [keyword, setKeyword] = useState('');
+  const [allProducts, setAllProducts] = useState([]);
+  //const [searchInput, setSearchInput] = useState("")
   useEffect(() => {
     
     fetch("https://reqres.in/api/products")
@@ -24,16 +30,53 @@ const DataAPI = () => {
     .then((dane) => {
   
        setProducts(dane.data)
+       
 })
     }, [])
-
+    const updateKeyword = (e:React.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      const filtered = products.filter(product => {
+       return `${product.name.toLowerCase()} ${product.id}`.includes(keyword);
+       
+      })
+      setKeyword(e.target.value);
+      setProducts(filtered);
+      if (keyword.length > 0) {
+        products.filter((product) => {
+        return product.name.match(keyword);
+    });
+    }
+   }
   return (
     <>
-  
-   {products.length !== 0 &&
+    <button onClick={refreshPage}>Click to clear the filter</button>
+       <div>
+      <input placeholder="Enter id" type="search" value={keyword}  onChange={updateKeyword} 
+       onKeyPress={(event) => {
+          if (!/[0-9]/.test(event.key)) {
+            event.preventDefault();
+          }
+        }} />
+      {
+      products.filter(product => {
+        if (query === '') {
+          return product;
+        } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
+          return product;
+        }
+      }).map((product, index) => (
+        <div className="box" key={index}>
+          <p>{product.name}</p>
+          <p>{product.id}</p>
+        </div>
+      ))
+    }
+    </div>
+
+   {/* {products.length !== 0 &&
     products.map((el, i) => {
       return <ProductsTab product = {el} key={i} />
-    })}
+    })} */}
    </>
    
   )
